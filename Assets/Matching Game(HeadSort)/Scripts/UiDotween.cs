b6847b052d6e091609f550ff6e0f9b104039e858
phani <80671431+phaniram2000@users.noreply.github.com>
2022-11-08 18:17:29 +0530

    Level order and audio in rundash

--- Assets/Matching Game(HeadSort)/Scripts/UiDotween.cs
+++ Assets/Matching Game(HeadSort)/Scripts/UiDotween.cs
@@ -11,8 +11,9 @@ public class UiDotween : MonoBehaviour
     {
         handTut.transform.DOLocalMove(new Vector3(32, -987, 0), 3).SetEase(Ease.OutQuint).SetLoops(-1,LoopType.Restart);
         handTut.transform.GetComponent<Image>().DOFade(0, 12).SetEase(Ease.Linear);
-        instruction.transform.DOShakePosition(1, new Vector3(15, 15, 15), 2, 90, fadeOut: true).SetEase(Ease.OutQuad)
-            .SetLoops(-1, LoopType.Incremental);
+        // instruction.transform.DOShakePosition(1, new Vector3(15, 15, 15), 2, 90, fadeOut: true).SetEase(Ease.OutQuad)
+        //     .SetLoops(-1, LoopType.Incremental);
+       
         instruction.transform.GetComponent<TextMeshProUGUI>().DOFade(0, 12).SetEase(Ease.Linear);
     }
 
